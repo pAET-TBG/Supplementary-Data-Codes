9f10b4cb500ba5be6705f82f769a4741daff156e
pAET-TBG <azzon0811@gmail.com>
2026-06-14 15:24:58 -0700

    Add files via upload

--- i_Calculate_displacement/Main_calculate_displacement.m
+++ i_Calculate_displacement/Main_calculate_displacement.m
@@ -284,4 +284,4 @@ quiver3(atomdown_sim(1,:), atomdown_sim(2,:), atomdown_sim(3,:), displacementDOW
 axis equal;
 view(90,30);
 hold off;
-save([pwd,'/output_data/atom_model_lower_layer_displacement_1_Convolution.mat'],'atomdown_sim','displacementDOWN')
+save([pwd,'/output_data/atom_model_lower_layer_displacement_1_convolution.mat'],'atomdown_sim','displacementDOWN')
