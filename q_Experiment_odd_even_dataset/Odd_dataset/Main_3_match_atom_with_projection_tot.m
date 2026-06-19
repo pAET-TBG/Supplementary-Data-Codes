975b86ed5c69ce36727e26dbb7a7a5a9d0dc0ff3
pAET-TBG <azzon0811@gmail.com>
2026-06-19 15:18:24 -0700

    Add files via upload

--- q_Experiment_odd_even_dataset/Odd_dataset/Main_3_match_atom_with_projection_tot.m
+++ q_Experiment_odd_even_dataset/Odd_dataset/Main_3_match_atom_with_projection_tot.m
@@ -4,7 +4,7 @@ clc;
 addpath([pwd,'/src/'])
 addpath([pwd,'/input_data/'])
 addpath([pwd,'/output_data/'])
-projs     = importdata('Projections_Odd.mat');
+projs     = importdata('Projections.mat');
 angles    = importdata('Angles.mat');
 atom_info = importdata('atom_tracing_manual_Odd_model.mat');
 atompos   = atom_info.atom;atompos(:,7805) = [];
@@ -79,8 +79,8 @@ for tempi = 1:size(projs,3)
 end
 
 atom = atomPos.*res; 
-save([pwd,'/output_data/atom_Refinement_Odd.mat'],'atom')
-save([pwd,'/output_data/label_Refinement_Odd.mat'],'label')
-save([pwd,'/output_data/Projs_Refinement_Odd.mat'],'cropProjs')
-save([pwd,'/output_data/angles_Refinement_Odd.mat'],'angles')
+save([pwd,'/output_data/atom_Refinement_Tot.mat'],'atom')
+save([pwd,'/output_data/label_Refinement_Tot.mat'],'label')
+save([pwd,'/output_data/Projs_Refinement_Tot.mat'],'cropProjs')
+save([pwd,'/output_data/angles_Refinement_Tot.mat'],'angles')
 
